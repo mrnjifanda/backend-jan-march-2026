0dc9934c5a808229976bb400494efde382f2fc3d
Mr NJIFANDA <mrnjifanda@gmail.com>
2026-01-22 17:45:25 +0100

    Add Update, Delete and middleware

--- 20-01-2026 Introduction to express/db.js
+++ 20-01-2026 Introduction to express/db.js
@@ -2,16 +2,20 @@ const USERS = [];
 
 const findById = (id) => {
     let user = null;
+    let index = null;
     for (let i = 0; i < USERS.length; i++) {
-        if (USERS[i].id === id) {
+
+        if (USERS[i].id == id) {
             user = USERS[i];
+            index = i;
             break;
         }
-
-        continue;
     }
 
-    return user;
+    // if (user && index) return { index, user };
+    if (user) return { index, user };
+
+    return null;
 };
 
 module.exports = { USERS, findById };
--- 20-01-2026 Introduction to express/main.js
+++ 20-01-2026 Introduction to express/main.js
@@ -7,19 +7,65 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/users", (request, response) => {
-    response.json(USERS);
-});
-
-app.post("/users", (req, res) => {
-
+const verifyCreateUserData = (req, res, next) => {
     const { id, name, phone } = req.body;
-    if (!id || !name || !phone) {
-        return res
-            .status(400)
-            .json({ massage: 'Please enter id, name and phone !!!'});
+    const errors = [];
+
+    if (!id) {
+        errors.push({
+            field: "id",
+            message: "Please enter id"
+        });
+    } else if (typeof id !== "number") {
+        errors.push({
+            field: "id",
+            message: "Please id must be a number"
+        });
     }
 
+    if (!name) {
+        errors.push({
+            field: "name",
+            message: "Please enter name"
+        });
+    } else if (name.length < 2 && name.length > 50) {
+        errors.push({
+            field: "name",
+            message: "Enter valid name (between 2 and 50 characters)"
+        });
+    }
+
+    if (!phone) {
+        errors.push({
+            field: "phone",
+            message: "Please enter phone"
+        });
+    } else if (phone.length !== 9) {
+        errors.push({
+            field: "phone",
+            message: "Enter valid phone (Phone must have exactly 9 numbers)"
+        });
+    } else if (!phone.startsWith(6)) {
+        errors.push({
+            field: "phone",
+            message: "Enter valid phone (Phone must start 6)"
+        });
+    }
+
+    if (errors.length !== 0) {
+        return res.status(400).json(errors);
+    }
+
+    next();
+}
+
+app.get("/users", (req, res) => {
+    res.json(USERS);
+});
+
+app.post("/users", verifyCreateUserData, (req, res) => {
+
+    const { id, name, phone } = req.body;
     USERS.push({ id, name, phone,
         created_at: new Date()
     })
@@ -30,13 +76,50 @@ app.post("/users", (req, res) => {
 app.get("/users/:id", (req, res) => {
     //  const id = req.params.id;
     const { id } = req.params;
-    
-    const user = findById(id);
-    if (user) {
-        return res.json(user);
+
+    const find = findById(id);
+    if (find) {
+        return res.json(find.user);
     }
 
     return res.status(404).json({ message: "User with id " + id + " not found" });
+});
+
+app.put("/users/:id", (req, res) => {
+
+    const { id } = req.params;
+    const { name, phone } = req.body;
+
+    const find = findById(id);
+    if (!find) {
+        return res.status(404)
+                  .json({ message: "User not found" });
+    }
+
+    const { index, user } = find;
+    if (name) {
+        user.name = name;
+    }
+    if (phone) {
+        user.phone = phone;
+    }
+
+    USERS[index] = user;
+    return res.json({ message:  "user updated successfully !!!" })
+});
+
+app.delete("/users/:id", (req, res) => {
+
+    const { id } = req.params;
+    const find = findById(id);
+    if (!find) {
+        return res.status(404)
+                  .json({ message: "User not found" });
+    }
+
+    const { index } = find;
+    delete USERS[index];
+    return res.json({ message:  "User deleted successfully !!!" })
 });
 
 app.listen(PORT, () => {
